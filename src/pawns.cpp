84528b00833016b1f318e1bdb46bbfe2830d12df
tomitank <tanky.hu@gmail.com>
2019-04-11 17:11:48 +0200

    Extended Backward (Bench: 3343915)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -106,7 +106,7 @@ namespace {
         // A pawn is backward when it is behind all pawns of the same color
         // on the adjacent files and cannot be safely advanced.
         backward =  !(ourPawns & pawn_attack_span(Them, s + Up))
-                  && (stoppers & (leverPush | (s + Up)));
+                  && ((stoppers & (leverPush | (s + Up))) || (ourPawns & (s + Up)));
 
         // Passed pawns will be properly scored in evaluation because we need
         // full attack info to evaluate them. Include also not passed pawns
